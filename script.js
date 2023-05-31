1156bfbc24999dc536fb47ef2d9b4b82bf14e1ad
arminaschenbrenner <armin.aschenbrenner@gmail.com>
2023-05-31 17:45:04 +0200

    fix: code cleanup

--- script.js
+++ script.js
@@ -1,198 +1,128 @@
-let startToggle = 0;
+// Intro pop up state variable
+let startToggle = false;
 
-let paperToggle = 0;
-let paperColor1 = [253, 246, 227];
-let paperColor2 = [255, 255, 255];
-let paperColor3 = [213, 181, 156];
+// DOM elements
+const intro = document.getElementById("intro-wrapper");
+const papers = document.getElementById("papers");
+const sizes = document.getElementById("sizes");
+const colors = document.getElementById("colors");
+const menus = [intro, papers, sizes, colors];
+
+// Paper color variables
+const paperColor1 = [253, 246, 227];
+const paperColor2 = [255, 255, 255];
+const paperColor3 = [213, 181, 156];
 let paperColor = paperColor3;
 
-let sizeToggle = 0;
-let penSize1 = 5;
-let penSize2 = 15;
-let penSize3 = 25;
+// Pen size variables
+const penSize1 = 5;
+const penSize2 = 15;
+const penSize3 = 25;
 let penSize = penSize2;
 
-let colorToggle = 0;
-let penColor1 = [0, 0, 0];
-let penColor2 = [255, 0, 0];
-let penColor3 = [255, 255, 0];
-let penColor4 = [0, 255, 0];
-let penColor5 = [0, 0, 255];
+// Pen color variables
+const penColor1 = [0, 0, 0];
+const penColor2 = [255, 0, 0];
+const penColor3 = [255, 255, 0];
+const penColor4 = [0, 255, 0];
+const penColor5 = [0, 0, 255];
 let penColor = penColor1;
+
+// Menu states variables
+const toggles = {
+  paper: false,
+  size: false,
+  color: false,
+};
 
 function setup() {
   // Including p5.js sketch: https://happycoding.io/tutorials/p5js/web-dev
   let canvas = createCanvas(296, 536);
-  canvas.parent("sketch-container");
+  canvas.parent("sketch-wrapper");
   background(paperColor);
   noStroke();
   fill(penColor);
 }
 
 function draw() {
+  // Draw when mouse is pressed only if menus are closed
   if (
-    mouseIsPressed === true &&
-    paperToggle == 0 &&
-    sizeToggle == 0 &&
-    colorToggle == 0 &&
-    startToggle == 1
+    mouseIsPressed &&
+    !toggles.paper &&
+    !toggles.size &&
+    !toggles.color &&
+    startToggle
   ) {
     circle(mouseX, mouseY, penSize);
   }
 }
 
+// Buttons functions
+
+// Start button
 function starting() {
-  document.getElementById("intro-wrapper").style.display = "none";
-  startToggle = 1;
+  menus[0].style.display = "none";
+  startToggle = true;
 }
 
+// Reset button
 function reseting() {
   background(paperColor);
   newPenColor = penColor;
   fill(newPenColor);
 }
 
+// Erase button
 function erasing() {
   let newPenColor = paperColor;
   fill(newPenColor);
 }
 
+// Draw button
 function drawing() {
   newPenColor = penColor;
   fill(newPenColor);
 }
 
-function paperMenuOn() {
-  if (startToggle == 1) {
-    document.getElementById("papers").style.display = "flex";
-    paperToggle = 1;
-    document.getElementById("sizes").style.display = "none";
-    sizeToggle = 0;
-    document.getElementById("colors").style.display = "none";
-    colorToggle = 0;
+// Open and close menus based on state
+function menu(_menu, _toggleName) {
+  if (startToggle) {
+    if (!toggles[_toggleName]) {
+      closeMenu();
+      menus[_menu].style.display = "flex";
+      toggles[_toggleName] = true;
+    } else if (toggles[_toggleName]) {
+      closeMenu();
+    }
   }
 }
 
-function paperMenuOff() {
-  document.getElementById("papers").style.display = "none";
-  paperToggle = 0;
+// Close all menus
+function closeMenu() {
+  menus[1].style.display = "none";
+  toggles.paper = false;
+  menus[2].style.display = "none";
+  toggles.size = false;
+  menus[3].style.display = "none";
+  toggles.color = false;
 }
 
-function paperMenu() {
-  if (paperToggle == 0) {
-    paperMenuOn();
-  } else if (paperToggle == 1) {
-    paperMenuOff();
-  }
-}
-
-function sizeMenuOn() {
-  if (startToggle == 1) {
-    document.getElementById("sizes").style.display = "flex";
-    sizeToggle = 1;
-    document.getElementById("papers").style.display = "none";
-    paperToggle = 0;
-    document.getElementById("colors").style.display = "none";
-    colorToggle = 0;
-  }
-}
-
-function sizeMenuOff() {
-  document.getElementById("sizes").style.display = "none";
-  sizeToggle = 0;
-}
-
-function sizeMenu() {
-  if (sizeToggle == 0) {
-    sizeMenuOn();
-  } else if (sizeToggle == 1) {
-    sizeMenuOff();
-  }
-}
-
-function colorMenuOn() {
-  if (startToggle == 1) {
-    document.getElementById("colors").style.display = "flex";
-    colorToggle = 1;
-    document.getElementById("sizes").style.display = "none";
-    sizeToggle = 0;
-    document.getElementById("papers").style.display = "none";
-    paperToggle = 0;
-  }
-}
-
-function colorMenuOff() {
-  document.getElementById("colors").style.display = "none";
-  colorToggle = 0;
-}
-
-function colorMenu() {
-  if (colorToggle == 0) {
-    colorMenuOn();
-  } else if (colorToggle == 1) {
-    colorMenuOff();
-  }
-}
-
-function paperChange1() {
-  paperColor = paperColor1;
+// Paper color change button
+function paperChange(_paperColor) {
+  paperColor = _paperColor;
   background(paperColor);
-  paperMenuOff();
+  closeMenu();
 }
 
-function paperChange2() {
-  paperColor = paperColor2;
-  background(paperColor);
-  paperMenuOff();
+// Pen size change button
+function sizeChange(_penSize) {
+  penSize = _penSize;
+  closeMenu();
 }
 
-function paperChange3() {
-  paperColor = paperColor3;
-  background(paperColor);
-  paperMenuOff();
-}
-
-function sizeChange1() {
-  penSize = penSize1;
-  sizeMenuOff();
-}
-
-function sizeChange2() {
-  penSize = penSize2;
-  sizeMenuOff();
-}
-
-function sizeChange3() {
-  penSize = penSize3;
-  sizeMenuOff();
-}
-
-function colorChange1() {
-  penColor = penColor1;
+// Pen color change button
+function colorChange(_penColor) {
+  penColor = _penColor;
   fill(penColor);
-  colorMenuOff();
-}
-
-function colorChange2() {
-  penColor = penColor2;
-  fill(penColor);
-  colorMenuOff();
-}
-
-function colorChange3() {
-  penColor = penColor3;
-  fill(penColor);
-  colorMenuOff();
-}
-
-function colorChange4() {
-  penColor = penColor4;
-  fill(penColor);
-  colorMenuOff();
-}
-
-function colorChange5() {
-  penColor = penColor5;
-  fill(penColor);
-  colorMenuOff();
+  closeMenu();
 }
